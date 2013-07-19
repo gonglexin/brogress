7d9a802b050250c365de237782e57622aaae2b65
gonglexin <gonglexin@gmail.com>
2013-07-19 11:05:00 +0800

    some style fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :name,  presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :name,  presence:   true,
+                    uniqueness: true,
+                    length:     { minimum: 2, maximum: 20 }
+
+  validates :email, presence:   true,
+                    uniqueness: true,
+                    format:     { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
 end
