4bd8c9142c0bd917e8736768741be614d6c22706
gonglexin <gonglexin@gmail.com>
2013-07-26 00:27:16 +0800

    page style refactor

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,12 +1,14 @@
 module ItemsHelper
   def item_image_tag(item, options = {})
+    image_service = 'http://placekitten.com/'
+    #image_service = 'http://placehold.it/'
     if item.image.url
       link_to(image_tag(item.image, options), item)
     else
       if item.id
-        link_to(image_tag("http://placekitten.com/#{140 + item.id}", options), item)
+        link_to(image_tag("#{image_service}#{140 + item.id}", options), item)
       else
-        link_to(image_tag("http://placekitten.com/#{rand(140..240)}", options), new_item_path)
+        link_to(image_tag("#{image_service}#{rand(140..240)}", options), new_item_path)
       end
     end
   end
